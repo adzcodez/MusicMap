9b9f7d6a692f74a39791e6aeeab1c15bdf1bf121
adzcodez <adamdeharo@gmail.com>
2021-03-05 14:21:49 +0000

    init

---
+++ app.R
@@ -0,0 +1,108 @@
+' Author: Adam de Haro '
+' Main app to be run for MusicMap '
+
+library(shiny)
+library(shinydashboard)
+library(DSpoty)
+library(leaflet)
+library(musicbrainz)
+library(dplyr)
+library(ggmap)
+library(htmltools)
+library(usethis)
+source("utils.R")
+source("credentials.R")
+
+# Git options
+use_git_config(user.name = "adzcodez", user.email = "adamdeharo@gmail.com")
+
+# Shiny options
+options(shiny.maxRequestSize=10*1024^2) # Increases file size upload limit
+
+# Access key for Google Maps API
+key <- return_google_key()
+register_google(key = key)
+
+# Generate access token for Spotify API
+client_id <- return_spotify_client()
+client_secret <- return_spotify_secret()
+redirect_uri <- ''
+Sys.setenv(client_id = client_id)
+Sys.setenv(client_secret = client_secret)
+token <- DSpoty::get_spotify_access_token(
+    client_id = Sys.getenv("client_id"),
+    client_secret = Sys.getenv("client_secret")
+)
+
+# # UI
+# ui <- fluidPage(
+#     titlePanel("Spotify Map"),
+#     sidebarLayout(
+#         sidebarPanel(
+#             fileInput("scrobblescsv", "Upload scrobbles .csv here")
+#         ), 
+#         mainPanel(
+#            tableOutput("scrobbles_df")
+#         )
+#     )
+# )
+    
+ui <- dashboardPage(
+    
+)
+
+
+# Server
+server <- function(input, output) {
+    scrobbles_upload <- reactive({
+        req(input$scrobblescsv, file.exists(input$scrobblescsv$datapath))
+        read.csv(input$scrobblescsv$datapath, header=FALSE, col.names=c("Artist", "Album", "Track", "Time"))
+    })
+    scrobbles_preprocessed <- reactive({ # Produces preprocessed scrobbles dataframe
+        scrobbles <- scrobbles_upload() # Checks file is uploaded
+        scrobbles <- read.csv(input$scrobblescsv$datapath, header=FALSE, col.names=c("Artist", "Album", "Track", "Time")) # Reads the file
+        scrobbles <- data.frame(scrobbles[1:3], apply(scrobbles[4], 2, processTime))
+        scrobbles <- rename(scrobbles, Day=4, Month=5, Year=6, Hour=7, Minute=8)
+        scrobbles <- scrobbles[scrobbles$Year > 1970,]
+         })
+    scrobbles_artist_plays <- reactive({ # Produces artist plays dataframe
+        req(scrobbles_preprocessed())
+        scrobbles_artist_plays <- scrobbles_preprocessed() %>% 
+                                  count(Artist, sort=TRUE) %>% 
+                                  filter(n > 1500)
+    })
+    scrobbles_artist_info <- reactive({
+        req(scrobbles_artist_plays())
+        rows = dim(scrobbles_artist_plays())[1]
+        cols = 5
+        artist_info <- matrix(ncol=cols, nrow=rows)
+        for (i in 1:rows) {
+            artist_name <- scrobbles_artist_plays()$Artist[i]
+            print(artist_name)
+            artist_id <- pull_artist_id(artist_name)
+            artist_df <- lookup_artist_by_id(artist_id, includes="tags")
+            artist_info[i,1] <- artist_name # Artist name
+            artist_info[i,2] <- pull_country(artist_df) # Artist country
+            artist_info[i,3] <- pull_area(artist_df) # Artist area
+            artist_info[i,4] <- pull_year(artist_df, artist_id) # StartYear
+            artist_info[i,5] <- pull_genre(artist_df) # Genre
+        }
+        scrobbles_artist_info <- as.data.frame(artist_info)
+        scrobbles_artist_info <- rename(scrobbles_artist_info, Artist=1, Country=2, Area=3, StartYear=4, Genre=5)
+    })
+    scrobbles_coordinates <- reactive({
+        req(scrobbles_artist_info())
+        scrobbles_coordinates <- unique(scrobbles_artist_info()[c("Country", "Area")])
+        scrobbles_coordinates["Latitude"] = NA; scrobbles_coordinates["Longitude"] = NA
+        scrobbles_coordinates <- mutate(scrobbles_coordinates, address=paste(Area, Country))
+        addresses <- scrobbles_coordinates$address 
+        geocodes <- geocode(addresses, source = "google")
+        scrobbles_coordinates <- mutate(scrobbles_coordinates, Latitude=geocodes$lat, Longitude=geocodes$lon)
+        scrobbles_coordinates <- scrobbles_coordinates[1:4]
+    })
+    output$scrobbles_df <- renderTable({
+        scrobbles_coordinates()
+    })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ utils.R
@@ -0,0 +1,70 @@
+' Author: Adam de Haro '
+' Utility functions for MusicMap '
+
+# Functions for preprocessing scrobbles csv
+processTime <- function(input_time){
+  ' Returns a list in the format [day, month, year, hour, minute] '
+  time <- as.POSIXlt(input_time, format="%d %b %Y %H:%M")
+  
+  day <- time$mday
+  month <- (time$mon + 1) # Months are 0-indexed
+  year <- (time$year + 1900)  # Years since 1900
+  hour <- time$hour
+  min <- time$min
+  
+  new_time <- list(day, month, year, hour, min)
+  
+  return(new_time)
+}
+
+# Functions for populating artist info dataframe 
+
+pull_artist_id <- function(artist) {
+  ' Retrieves query artist MBID for use in further queries ' 
+  id <- search_artists(artist) %>% 
+    select(mbid) %>% 
+    slice(1) %>% 
+    pull()
+  return(id)
+}
+
+pull_country <- function(df) {
+  country <- df$country
+  return(country)
+}
+
+pull_area <- function(df) {
+  area <- df$begin_area_name
+  return(area)
+}
+
+get_coords <- function(country, area){
+  
+  return(lat, long)
+}
+
+pull_year <- function(df, id) {
+  if (df$type == "Group"){ # If artist is a group, take year of formation
+    year <- df$life_span_begin
+  }
+  else { # If artist is a person, take year of first release
+    releases <- browse_release_groups_by("artist", id)
+    year <- sort(releases$first_releas_date)[1]
+  } 
+  return(year)
+}
+
+# I need this function to handle empty tags
+# TODO: Use LastFM API to improve genre selection
+pull_genre <- function(df) {
+  tags_list <- df$tags
+  if (is.null(unlist(tags_list))) {
+    return(NA)
+  }
+  tags_values <- as.numeric(unlist(tags_list[[1]][2]))
+  tags_genres <- tags_list[[1]][1]
+  genre <- tags_genres[which.max(tags_values),]
+  genre <- genre[1,1,1]
+  return(genre)
+  
+}
